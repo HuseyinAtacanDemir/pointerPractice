384ae9a83f7784371f0492bc9eccd5ef1062bdcd
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-02-13 14:09:01 +0300

    data structures initial commit

---
+++ dataStructures/practice1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//data structures are a way of storing and organazing data 
+//so that the data can be used efficiently
+
+int main(){
+
+    
+}
